cec5f0c29e0bdd9d9ebd88e2a066a2d8163c3e3e
Adam Wilson <aswilson87@gmail.com>
2020-12-30 21:05:32 -0600

    Updates

--- server/controllers/authControllers/authSignin.js
+++ server/controllers/authControllers/authSignin.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-const db = require('../../dbSetup.js');
-const bcrypt = require('bcrypt');
-=======
-const db = require('../../db.js');
-const bcrypt = require('bcryptjs');
->>>>>>> c00ce0c85d6f5611ad3421b949ee5e6ac8613a9b
+const bcrypt = require("bcryptjs");
+const db = require("../../db.js");
 
 const authSignin = (req, res, next) => {
   // write code here
@@ -12,7 +7,7 @@ const authSignin = (req, res, next) => {
   const qStr = `SELECT * FROM auth WHERE user_name = ${req.body.username};`;
   db.query(qStr).then((qres) => {
     const user = qres.rows[0] ? qres.rows[0] : false;
-    if (!user) res.redirect('/signup');
+    if (!user) res.redirect("/signup");
     bcrypt
       .compare(req.body.password, user.passkey)
       .then((result) => {
@@ -20,19 +15,19 @@ const authSignin = (req, res, next) => {
           res.locals.username = req.body.username;
           return next();
         }
-        return res.redirect('/signin');
+        return res.redirect("/signin");
       })
       .catch(() =>
         next({
-          log: 'authController.authSignin error',
-          message: { err: 'bcrypt failed' },
-        }),
+          log: "authController.authSignin error",
+          message: { err: "bcrypt failed" },
+        })
       )
       .catch(() =>
         next({
-          log: 'authController.authSignin error',
-          message: { err: 'SQL query failed' },
-        }),
+          log: "authController.authSignin error",
+          message: { err: "SQL query failed" },
+        })
       );
   });
 };
